ed859d55d7895dc2ddbfff582fd530aa58fc2a9b
Kim Roman <romankim94@gmail.com>
2024-10-13 23:12:38 +0300

    Last editions

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -193,14 +193,6 @@ class CommentUpdateView(
     pk_url_kwarg = 'comment_id'
     template_name = 'blog/comment.html'
 
-    def form_valid(self, form):
-        if (
-            self.request.user.is_anonymous
-            or self.request.user != self.object.author
-        ):
-            return self.get_success_url()
-        return super().form_valid(form)
-
     def get_queryset(self, post=None):
         return super().get_queryset().filter(
             author=self.request.user,
@@ -226,14 +218,6 @@ class CommentDeleteView(
     model = Comment
     pk_url_kwarg = 'comment_id'
     template_name = 'blog/comment.html'
-
-    def form_valid(self, form):
-        if (
-            self.request.user.is_anonymous
-            or self.request.user != self.object.author
-        ):
-            return self.get_success_url()
-        return super().form_valid(form)
 
     def get_queryset(self, post=None):
         return super().get_queryset().filter(
